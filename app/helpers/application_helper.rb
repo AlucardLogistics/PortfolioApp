2415d5f609fa91c7cd65531a10badbb84afa4736
Alucard Logistics <alucardlogistics@gmail.com>
2018-03-14 15:34:11 -0500

    implemented refactor for nav and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,4 +25,62 @@ module ApplicationHelper
     AlucardLogisticsViewTool::Renderer.copyright 'Dragos Stoian', 'All rights reserved'
   end
 
+  #create an array of key values of rake routes from nav and the title - link name shown in browser
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: "Home"
+      },
+      {
+        url: about_me_path,
+        title: "About Me"
+      },
+      {
+        url: contact_path,
+        title: "Contact"
+      },
+      {
+        url: blogs_path,
+        title: "Blog"
+      },
+      {
+        url: portfolios_path,
+        title: "Portfolio"
+      }
+    ]
+  end
+
+  #get nav_items, create a nav_links string and append << all the items
+  def new_nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe    
+  end
+
+  #defines the current page the user is on and sets "active" attribute to the nav link
+  #boolean method -> def active?
+  def active? path
+    "active" if current_page? path
+  end
+
+  #NAV will generate a string that will have all the lines as written inside addin \n after each one
+  #it has to start from the start of editor line (no white space) indentation as shown in this method
+  #nav_links.html_safe returns the whole string as a sanitized string
+  #this method is never used, is just for demo purpose
+  def nav_helper style, tag_type
+nav_links = <<NAV
+<#{tag_type}><a href="#{root_path}" class="#{style} #{active? root_path}">Home</a></#{tag_type}>
+<#{tag_type}><a href="#{about_me_path}" class="#{style} #{active? about_me_path}">About Me</a></#{tag_type}>
+<#{tag_type}><a href="#{contact_path}" class="#{style} #{active? contact_path}">Contact</a></#{tag_type}>
+<#{tag_type}><a href="#{blogs_path}" class="#{style} #{active? blogs_path}">Blog</a></#{tag_type}>
+<#{tag_type}><a href="#{portfolios_path}" class="#{style} #{active? portfolios_path}">Portfolio</a></#{tag_type}>
+NAV
+    nav_links.html_safe
+  end  
+
 end
