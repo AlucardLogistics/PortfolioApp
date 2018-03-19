fcf170d7900ac9161db51ad9606a7987efb2f246
Alucard Logistics <alucardlogistics@gmail.com>
2018-03-19 12:06:10 -0500

    Updated file upload forms and refactored palceholder functionality into view

--- app/models/concerns/placeholder.rb
+++ app/models/concerns/placeholder.rb
@@ -2,8 +2,8 @@ module Placeholder
   extend ActiveSupport::Concern
 
   #methods created here can be shared accross any models
-
-  def self.image_generator(height:, width:)
-    "http://via.placeholder.com/#{height}x#{width}"
-  end
+  #NO LONGER NEEDED AFTER CARRIERWAVE GEM WAS IMPLEMENTD
+  # def self.image_generator(height:, width:)
+  #   "http://via.placeholder.com/#{height}x#{width}"
+  # end
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,11 +7,11 @@ class Portfolio < ApplicationRecord
   accepts_nested_attributes_for :technologies, 
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
 
-  #this is a placeholder file created in the models/concerns/placeholder.rb
-  include Placeholder 
+  #this is a placeholder file created in the models/concerns/placeholder.rb NO LONGER NEEDED AFTER CARRIERWAVE GEM
+  #include Placeholder 
 
   #validation for the porfolio fields (can't be empty condition)
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  validates_presence_of :title, :body
 
   #carrierwave gem
   mount_uploader :thumb_image, PortfolioUploader
@@ -30,16 +30,18 @@ class Portfolio < ApplicationRecord
   #defining a scope that will filter the portfolio items based on their subtitle
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
 
+  #AFTER CARRIERWAVE GEM WAS ADDED IT WILL OVVERWRITE THE PLACEHOLDER PICS
   #defining default values of data
   #this will occur after the def new method in the controller
-  after_initialize :set_defaults
+  #after_initialize :set_defaults
 
   #sets the default image and thumb image as default in case is not chosen
   #||= this will set a default image if image holder is nill (it will not overwrite an image if is already beeing set)
-  def set_defaults
-    #image_generator def created in the controllers/concerns/placeholder.rb
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-  end
+  # def set_defaults
+  #   #image_generator def created in the controllers/concerns/placeholder.rb
+  #   self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+  #   self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+  # end
+  #END PLACEHOLDER IMPLMENTATION BEFORE CARRIERWAVE  
 end
 
