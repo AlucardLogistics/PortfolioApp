8da5dfefff34858ead16480e8cbffaf802cd4bde
Alucard Logistics <alucardlogistics@gmail.com>
2018-03-24 14:04:53 -0500

    implemented radis gem and added comment table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :comments
   #resources for users and custom URL for sign_in, sing_out and sign_up actions
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
 
