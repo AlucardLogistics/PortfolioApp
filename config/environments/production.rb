bcd6682a8a3c1121cbb1f7c5401d6b693ee5d885
Alucard Logistics <alucardlogistics@gmail.com>
2018-04-01 18:45:49 -0500

    configured actioncable for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://alucardlogistics.herokuapp.com', 'http://alucardlogistics.herokuapp.com']
+  config.action_cable.url = "wss://alucardlogistics.herokuapp.com/cable"
 end
